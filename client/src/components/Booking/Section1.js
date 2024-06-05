c0b33981f86a2d1fe341f66969d79040f90489e4
asyrafazman <210262@student.upm.edu.my>
2024-06-05 11:30:36 +0800

    ServiceType Component
    break it the first page of booking by coomponents and update it to call on booking screen

---
+++ client/src/components/Booking/Section1.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Section1.css'
+
+
+function Section1() {
+    return (
+        <div className='textSection'>
+            <h1>Keep Your Journey Smooth</h1>
+            <h2>Drive with Confidence</h2>
+            <h3>Transparent Pricing</h3>
+        </div>
+    )
+}
+
+export default Section1;
--- client/src/screens/Booking.js
+++ client/src/screens/Booking.js
@@ -2,11 +2,15 @@
 import React from "react";
 import Navbar from "../components/HomePage/Navbar";
 import ServiceType from '../components/Booking/ServiceType';
+import Section1 from "../components/Booking/Section1";
 
 function Booking1() {
   return (
     <div className="Booking1">
       <Navbar />
+      <div className="Section1">
+        <Section1 />
+      </div>
       <div className="ServiceType">
         <ServiceType />
       </div>
